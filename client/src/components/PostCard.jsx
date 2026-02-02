f111c085634138343a1ccd4495903c5d7f20634c
Soumyajeet Das <soumyajeetdas@Soumyajeets-Mac-mini.local>
2026-02-03 03:43:01 +0530

    discover profile connections created

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,7 +2,7 @@ import moment from 'moment'
 import React, { useState } from 'react'
 import { BadgeCheck, ThumbsUp, MessageCircle, Share2 } from 'lucide-react'
 import { dummyUserData } from '../assets/assets';
-
+import { useNavigate } from 'react-router-dom';
  
  const PostCard = ({post}) => {
 
@@ -17,13 +17,13 @@ import { dummyUserData } from '../assets/assets';
     const handleLike =async () => {
     };
 
-
+const navigate = useNavigate();
 
 
    return (
      <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
         {/* User Info */}
-        <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <div onClick={()=> navigate( '/profile/'+ post.user._id )}className='inline-flex items-center gap-3 cursor-pointer'>
             <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow' />
             <div>
                 <div className=' flex items-center space-x-1'>
